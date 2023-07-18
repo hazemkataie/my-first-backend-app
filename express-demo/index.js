27a0236258ead83dfc338570a6190c3d3667c9fb
Hazem Kataie <hazem.kataie@stu.fbu.edu.tr>
2023-07-18 13:23:15 +0300

    Update

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,14 +1,43 @@
+//  INCLUDE EXPRESS
 const express = require('express');
-
 const app = express();
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+];
 
+//  GET REQUESTS
 app.get('/', (req,res) => {
 
-    res.send('Hello World');
+    res.send('Hello World!!!');
 
 });
+
+app.get('/api/courses', (req,res) => {
+    res.send(courses);
+});
+
+app.get('/api/courses/:id',(req,res) => {
+    const course = courses.find(c => c.id == parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given ID was not found') //404
+    res.send(course);
+});
+
+// app.get('/api/posts/:year/:month',(req,res) => {
+//     res.send(req.params);
+// });
+
+// app.get('/api/posts/:year/:month',(req,res) =>{
+//     res.send(req.query);
+// });
+
+//  OTHER REQUESTS
 // app.post();
 // app.put();
 // app.delete();
 
-app.listen(3000,()=> console.log('Listening on port 3000...'));
+
+//  PORT
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}...`));
