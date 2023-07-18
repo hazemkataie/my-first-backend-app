f6925e1e0a1053606d43c9d8f84b52e633530ff8
Hazem Kataie <hazem.kataie@stu.fbu.edu.tr>
2023-07-18 16:00:18 +0300

    Create new service

--- express-demo/index.js
+++ express-demo/index.js
@@ -5,13 +5,30 @@ const app = express();
 app.use(express.json());
 
 
+
+
+//SERVICES
+
 const courses = [
     { id: 1, name: 'course1'},
     { id: 2, name: 'course2'},
     { id: 3, name: 'course3'},
 ];
 
+const genres = [
+    { id: 1, name: 'Action'},
+    { id: 2, name: 'Drama'},
+    { id: 3, name: 'Comedy'},
+];
+
+
+
+
+
+
+
 //  GET REQUESTS
+
 app.get('/', (req,res) => {
 
     res.send('Hello World!!!');
@@ -28,13 +45,20 @@ app.get('/api/courses/:id',(req,res) => {
     res.send(course);
 });
 
-// app.get('/api/posts/:year/:month',(req,res) => {
-//     res.send(req.params);
-// });
+app.get('/api/genres',(req,res) => {
+    res.send(genres);
+})
 
-// app.get('/api/posts/:year/:month',(req,res) =>{
-//     res.send(req.query);
-// });
+app.get('/api/genres/:id',(req,res) => {
+    const genre = genres.find(c => c.id == parseInt(req.params.id));
+    if(!genre) return res.status(404).send('The course with the given ID was not found') //404
+    res.send(genre);
+});
+
+
+
+
+
 
 //  POST REQUESTS
 app.post('/api/courses',(req,res) => {
@@ -48,6 +72,25 @@ app.post('/api/courses',(req,res) => {
     courses.push(course);
     res.send(course);
 });
+
+app.post('/api/genres',(req,res) => {
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length+1,
+        name: req.body.name
+    }
+
+    genres.push(genre);
+    res.send(genre);
+});
+
+
+
+
+
+
 
 //  PUT REQUESTS
 app.put('/api/courses/:id',(req,res) => {
@@ -64,14 +107,25 @@ app.put('/api/courses/:id',(req,res) => {
     res.send(course);
 });
 
+app.put('/api/genres/:id',(req,res) => {
+    const genre = genres.find(c => c.id == parseInt(req.params.id));
+    if(!genre) return  res.status(404).send('The genre with the given ID was not found') //404
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-function validateCourse(course){
-    const schema = {
-        name: Jois.string().min(3).required()
-    }
+    genre.name = req.body.name;
+    res.send(genre);
+});
 
-    return Jois.validate(course, schema);
-}
+
+
+
+
+
+
+
+
+
 
 //  DELETE REQUESTS
 app.delete('/api/courses/:id',(req,res) => {
@@ -87,6 +141,40 @@ app.delete('/api/courses/:id',(req,res) => {
     //return the same course
     res.send(course); 
 });
+
+
+app.delete('/api/genres/:id',(req,res) => {
+    const genre = genres.find(c => c.id == parseInt(req.params.id));
+    if(!genre) return res.status(404).send('The course with the given ID was not found')
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+
+    res.send(genre);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function validateCourse(course){
+    const schema = {
+        name: Jois.string().min(3).required()
+    }
+
+    return Jois.validate(course, schema);
+}   
+
 
 
 //  PORT
