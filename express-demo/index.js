5f60211d539e99407a5fc5d6ab756e115a711912
Hazem Kataie <hazem.kataie@stu.fbu.edu.tr>
2023-07-18 15:37:47 +0300

    Updates

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,10 @@
 //  INCLUDE EXPRESS
+const Jois = require('joi');
 const express = require('express');
 const app = express();
+app.use(express.json());
+
+
 const courses = [
     { id: 1, name: 'course1'},
     { id: 2, name: 'course2'},
@@ -20,7 +24,7 @@ app.get('/api/courses', (req,res) => {
 
 app.get('/api/courses/:id',(req,res) => {
     const course = courses.find(c => c.id == parseInt(req.params.id));
-    if(!course) res.status(404).send('The course with the given ID was not found') //404
+    if(!course) return res.status(404).send('The course with the given ID was not found') //404
     res.send(course);
 });
 
@@ -32,10 +36,57 @@ app.get('/api/courses/:id',(req,res) => {
 //     res.send(req.query);
 // });
 
-//  OTHER REQUESTS
-// app.post();
-// app.put();
-// app.delete();
+//  POST REQUESTS
+app.post('/api/courses',(req,res) => {
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const course = {
+        id: courses.length+1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+});
+
+//  PUT REQUESTS
+app.put('/api/courses/:id',(req,res) => {
+    //Look up the course
+    //If not existing, return 404
+    const course = courses.find(c => c.id == parseInt(req.params.id));
+    if(!course) return  res.status(404).send('The course with the given ID was not found') //404
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //Update course
+    course.name = req.body.name;
+    //Return the updated course
+    res.send(course);
+});
+
+
+function validateCourse(course){
+    const schema = {
+        name: Jois.string().min(3).required()
+    }
+
+    return Jois.validate(course, schema);
+}
+
+//  DELETE REQUESTS
+app.delete('/api/courses/:id',(req,res) => {
+    //Look up the course
+    //No existing, return 404
+    const course = courses.find(c => c.id == parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with the given ID was not found')
+
+    //Delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    //return the same course
+    res.send(course); 
+});
 
 
 //  PORT
